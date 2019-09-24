8d0d7f1dbfe0050b2c8ff604ed96bed587bd3e48
Mauricio Muratori <maurimura@gmail.com>
2019-09-24 02:25:58 -0300

    Updates

---
+++ precache-manifest.d5116e1ad9d9cb3a214e45dee37bfc49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959a95356183f1996cf42303554e4cde",
+    "url": "/cocktailFinder/index.html"
+  },
+  {
+    "revision": "d875c0ad5dd50745d2cd",
+    "url": "/cocktailFinder/static/css/main.c7f023a9.chunk.css"
+  },
+  {
+    "revision": "8b6814bc4d359654840b",
+    "url": "/cocktailFinder/static/js/2.2eac5c42.chunk.js"
+  },
+  {
+    "revision": "d875c0ad5dd50745d2cd",
+    "url": "/cocktailFinder/static/js/main.344d1bfc.chunk.js"
+  },
+  {
+    "revision": "1041028e04768c72239f",
+    "url": "/cocktailFinder/static/js/runtime-main.59b3556e.js"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/cocktailFinder/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "879518a5ad98bcea6498d13dbb1eecaf",
+    "url": "/cocktailFinder/static/media/logo.879518a5.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/cocktailFinder/static/media/search.a87eca9e.svg"
+  }
+]);
